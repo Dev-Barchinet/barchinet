a547843aad70ced6534f407220a5a22c961f2fc0
amu-ghodrat <abgkcode@gmail.com>
2024-11-04 08:37:40 +0330

    fix: configed app to run on 3001 port

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "architect",
+      script: "npm start",
+      args: "--max-old-space-size=4096 --trace-deprecation",
+      cwd: "./",
+      watch: true,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        JWT_SECRET: "please dont hack us",
+        NEXTAUTH_SECRET: "please dont hack us",
+        NEXTAUTH_URL: "http://localhost:3001",
+        NEXT_PUBLIC_GATEWAY: "https://api.barchinet.com:8443",
+        GOOGLE_ID:
+          "152686576151-7bp2vue0othm0hoj94nqdj5odbd252rh.apps.googleusercontent.com",
+        GOOGLE_SECRET: "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",
+      },
+    },
+  ],
+};
