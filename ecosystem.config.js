a47ae5353f3f1e4e2d4feeef137b0b608ae1bff0
amu-ghodrat <abgkcode@gmail.com>
2024-11-04 12:41:24 +0330

    fix: add port to deploy config, handled next auth url

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       exec_mode: "cluster",
       instances: 1, // Or a number of instances
       script: "node_modules/next/dist/bin/next",
-      args: "start",
+      args: "start -p 3001",
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
@@ -14,7 +14,7 @@ module.exports = {
         NEXT_PUBLIC_APP_BASE_API_URL: "https://api.barchinet.com:8443/",
         JWT_SECRET: "please dont hack us",
         NEXTAUTH_SECRET: "please dont hack us",
-        NEXTAUTH_URL: "http://localhost:3001",
+        NEXTAUTH_URL: "https://architect.barchinet.com",
         NEXT_PUBLIC_GATEWAY: "https://api.barchinet.com:8443",
         GOOGLE_ID:
           "152686576151-7bp2vue0othm0hoj94nqdj5odbd252rh.apps.googleusercontent.com",
@@ -25,7 +25,7 @@ module.exports = {
         NEXT_PUBLIC_APP_BASE_API_URL: "https://api.barchinet.com:8443/",
         JWT_SECRET: "please dont hack us",
         NEXTAUTH_SECRET: "please dont hack us",
-        NEXTAUTH_URL: "http://localhost:3001",
+        NEXTAUTH_URL: "https://architect.barchinet.com",
         NEXT_PUBLIC_GATEWAY: "https://api.barchinet.com:8443",
         GOOGLE_ID:
           "152686576151-7bp2vue0othm0hoj94nqdj5odbd252rh.apps.googleusercontent.com",
